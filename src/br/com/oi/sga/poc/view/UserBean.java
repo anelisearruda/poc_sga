70f69207101f68236c89bc0221a13a5b9a5d7498
Anelise Arruda da Gama <agama@ig.com>
2012-08-17 16:50:52 -0300

    combos

--- src/br/com/oi/sga/poc/view/UserBean.java
+++ src/br/com/oi/sga/poc/view/UserBean.java
@@ -1,11 +1,12 @@
 package br.com.oi.sga.poc.view;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
 
 import br.com.sga.poc.model.User;
 
@@ -15,13 +16,28 @@ public class UserBean {
 
 	private User user;
 	
-	private List<String> genderList;
-	private List<String> stateList;
-	private List<String> cityList;
+	private List<SelectItem> genderList;
+	private List<SelectItem> stateList;
+	private List<SelectItem> cityList;
+	
+	private String currentGender;
 
 	@PostConstruct
 	public void doInit() {
 		user = new User();
+		
+		genderList = new ArrayList<SelectItem>();
+		genderList.add(new SelectItem("F","F"));
+		genderList.add(new SelectItem("M","F"));
+		
+		stateList = new ArrayList<SelectItem>();
+		stateList.add(new SelectItem("MG","MG"));
+		stateList.add(new SelectItem("RJ","RJ"));
+		stateList.add(new SelectItem("SP","SP"));
+		
+		cityList = new ArrayList<SelectItem>();
+		cityList.add(new SelectItem("Cidade 1","Cidade 1"));
+		cityList.add(new SelectItem("Cidade 2","Cidade 2"));
 	}
 	
 	public String send() {
@@ -41,28 +57,36 @@ public class UserBean {
 		this.user = user;
 	}
 
-	public List<String> getGenderList() {
+	public List<SelectItem> getGenderList() {
 		return genderList;
 	}
 
-	public void setGenderList(List<String> genderList) {
+	public void setGenderList(List<SelectItem> genderList) {
 		this.genderList = genderList;
 	}
 
-	public List<String> getStateList() {
+	public List<SelectItem> getStateList() {
 		return stateList;
 	}
 
-	public void setStateList(List<String> stateList) {
+	public void setStateList(List<SelectItem> stateList) {
 		this.stateList = stateList;
 	}
 
-	public List<String> getCityList() {
+	public List<SelectItem> getCityList() {
 		return cityList;
 	}
 
-	public void setCityList(List<String> cityList) {
+	public void setCityList(List<SelectItem> cityList) {
 		this.cityList = cityList;
 	}
 
+	public String getCurrentGender() {
+		return currentGender;
+	}
+
+	public void setCurrentGender(String currentGender) {
+		this.currentGender = currentGender;
+	}
+	
 }
