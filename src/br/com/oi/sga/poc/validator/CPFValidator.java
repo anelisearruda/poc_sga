f9f595739c95d1c7a1f0882b94281a1f97e7533f
Anelise Arruda da Gama <agama@ig.com>
2012-08-20 15:41:12 -0300

    Implementação do Converter

--- src/br/com/oi/sga/poc/validator/CPFValidator.java
+++ src/br/com/oi/sga/poc/validator/CPFValidator.java
@@ -7,9 +7,11 @@ import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
+@FacesValidator("cpfValidator")
 public class CPFValidator implements Validator {
 
 	@Override
